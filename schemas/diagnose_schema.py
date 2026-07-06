e27a03bc8405395210d9bb50702bae035cdab7bd
callaing0 <0hmong0212@gmail.com>
2026-07-06 12:07:05 +0900

    feat: 가상 메이크업 및 컬러 서비스 코드 수정

---
+++ schemas/diagnose_schema.py
@@ -0,0 +1,23 @@
+# schemas/diagnose_schema.py
+
+from pydantic import BaseModel
+from typing import List
+
+from schemas.generated_file_schema import ROIResponse
+
+class DiagnoseResponse(BaseModel):
+    message: str
+
+    original_image_id: int
+
+    files: List[ROIResponse]
+
+    personal_color: str
+
+    detected_skin_hex: str
+
+    original_image_id: int
+
+    diagnosis_confidence: int
+
+    original_image_url: str
